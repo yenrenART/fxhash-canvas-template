596ea222996d13b01673e6c8e6fe7186418eadb4
yenren <nailyener@hotmail.com>
2022-03-02 15:14:20 +0300

    Update functions.js

--- functions.js
+++ functions.js
@@ -6,9 +6,10 @@ function randomNumber(min, max, fxrand) {
 // Rarity picker
 function rarityPicker(values, fxrand) {
 	var weight = values[0].w;
+	const sumWeights = values.reduce((p, c) => p+c.w, 0) || 100;
 	
 	for (var i = 0; i < values.length; i++) {
-		if (fxrand < weight / 100) {
+		if (fxrand < weight / sumWeights) {
 			return values[i].v;
 		}
 		weight += values[i + 1].w;
