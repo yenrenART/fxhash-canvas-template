ee8b63463df40ceab644781dbee457e19a2e7849
yenren <nailyener@hotmail.com>
2022-03-02 13:16:58 +0300

    Create functions.js

---
+++ functions.js
@@ -0,0 +1,16 @@
+// Random number between min/max
+function randomNumber(min, max, fxrand) {
+	return Math.floor(fxrand * (max - min + 1) + min);
+}
+
+// Rarity picker
+function rarityPicker(values, fxrand) {
+	var weight = values[0].w;
+	
+	for (var i = 0; i < values.length; i++) {
+		if (fxrand < weight / 100) {
+			return values[i].v;
+		}
+		weight += values[i + 1].w;
+	}
+}
