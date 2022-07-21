436fb549c9cee9d6eadc6608f0e7d2c38bab1373
yenren <nailyener@hotmail.com>
2022-07-21 20:14:35 +0300

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,3 +1,5 @@
+const canvas = document.querySelector('canvas'), ctx = canvas.getContext('2d');
+
 // Random number between min/max (both inclusive)
 function randomNumber(min, max, fxrand) {
 	return Math.floor(fxrand * (max - min + 1) + min);
@@ -6,6 +8,16 @@ function randomNumber(min, max, fxrand) {
 // Random array item
 function randomItem(arr, fxrand) {
 	return arr[Math.floor(fxrand * arr.length)];
+}
+
+// Random n array items
+function randomItems(arr, n, fxrand) {
+	return arr.map(x => ({ x, r: fxrand })).sort((a, b) => a.r - b.r).map(a => a.x).slice(0, n);
+}
+
+// Random color (HEX)
+function randomColor() {
+	return Math.random().toString(16).substr(2, 6);
 }
 
 // Rarity picker
