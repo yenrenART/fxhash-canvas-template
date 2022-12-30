2cdb9f1d0a5b04a88d043f57444246c4a3af963a
yenren <nailyener@hotmail.com>
2022-12-30 10:26:14 +0300

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,21 +1,35 @@
+var canvas = document.querySelector('canvas'), ctx = canvas.getContext('2d', { alpha:false });
+var save = location.search.split('s=')[1];
+
+// Shorten fxrand() to fx()
+function fx() { return fxrand() }
+
 // Random number between min/max (both inclusive)
-function randomNumber(min, max, fxrand) {
-	return Math.floor(fxrand * (max - min + 1) + min);
+function randomNumber(min, max) {
+	return Math.floor(fx() * (max - min + 1) + min);
 }
 
 // Random array item
-function randomItem(arr, fxrand) {
-	return arr[Math.floor(fxrand * arr.length)];
+function randomItem(arr) {
+	return arr[Math.floor(fx() * arr.length)];
 }
 
 // Random n array items
-function randomItems(arr, n, fxrand) {
-	return arr.map(x => ({ x, r: fxrand })).sort((a, b) => a.r - b.r).map(a => a.x).slice(0, n);
+function randomItems(arr, n) {
+	return arr.map(x => ({ x, r: fx() })).sort((a, b) => a.r - b.r).map(a => a.x).slice(0, n);
+}
+
+// Shuffle array
+function shuffle(arr) {
+	for (let i = arr.length - 1; i > 0; i--) {
+		let n = Math.floor(fx() * (i + 1));
+		[arr[i], arr[n]] = [arr[n], arr[i]];
+	}
 }
 
 // Random HEX color
-function randomColor(fxrand) {
-	return fxrand.toString(16).substr(2, 6);
+function randomColor() {
+	return fx().toString(16).substr(2, 6);
 }
 
 // HEX to RGB
@@ -28,7 +42,7 @@ function rgbToHex(r, g, b) {
 	return ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
 }
 
-// Color gradient - c1 and c2 are HEX colors, n is number of colors in gradient
+// Color gradient - c1, c2 HEX colors, n number of gradient colors
 function colorGradient(c1, c2, n) {
 	let c1RGB = hexToRGB(c1);
 	let c2RGB = hexToRGB(c2);
@@ -44,14 +58,26 @@ function colorGradient(c1, c2, n) {
 }
 
 // Rarity picker
-function rarityPicker(values, fxrand) {
+function rarityPicker(values) {
 	var weight = values[0].w;
 	const sumWeights = values.reduce((p, c) => p+c.w, 0) || 100;
 	
 	for (var i = 0; i < values.length; i++) {
-		if (fxrand < weight / sumWeights) {
+		if (fx() < weight / sumWeights) {
 			return values[i].v;
 		}
 		weight += values[i + 1].w;
 	}
+}
+
+// Setup canvas size
+function setup(size) {
+	let w = innerWidth, h = innerHeight;
+	save = save ?? size;
+	let s = save / cw;
+	
+	canvas.width = save; canvas.height = save / ar;
+	canvas.style.width = ((w < h * ar) ? w : h * ar)+'px'; canvas.style.height = ((w < h * ar) ? w / ar : h)+'px';
+	canvas.style.display = 'block';
+	ctx.scale(s, s);
 }
