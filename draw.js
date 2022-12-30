e8c7f13a47fa9b185730c6dbcb00adcb1047597b
yenren <nailyener@hotmail.com>
2022-12-30 10:51:40 +0300

    Update draw.js

--- draw.js
+++ draw.js
@@ -1,38 +1,30 @@
-// Rarities
+// Aspect Ratio - Use one ar option below with any values you want
+var ar = 1; // Fixed ar
+var ar = randomItem([0.8, 1, 1.25]); // Varying ar
+
+// Rarities - Use of rarities is optional
 const pointsR = [{v:1,w:15},{v:2,w:25},{v:3,w:25},{v:4,w:35}];
 
 // Features
-var nPoints = rarityPicker(pointsR, fxrand());
+var nPoints = rarityPicker(pointsR);
+
+// Base Units - 1000 is base units for the canvas width, use any value you want
+var cw = 1000, ch = cw / ar;
 
 function draw() {
-	fxrand = sfc32(...hashes);
+	// Default Canvas Size - 1200px width, use any value you want
+	fxrand = sfc32(...hashes); setup(1200);
 	
-	const canvas = document.querySelector('canvas'), ctx = canvas.getContext('2d');
-	
-	// Use one of the canvas size options:
-	// 1) Square
-	let cw = canvas.width = canvas.height = Math.min(window.innerWidth, window.innerHeight);
-	
-	// 2) Aspect ratio
-	let cw, ch, ar = 0.75;
-	if (window.innerWidth < ar * window.innerHeight) {
-		cw = canvas.width = window.innerWidth; ch = canvas.height = cw / ar;
-	} else {
-		ch = canvas.height = window.innerHeight; cw = canvas.width = ch * ar;
-	}
-	
-	// Size unit
-	let size = 1000;
-	let u = cw / size;
-		
 	// Draw your artwork here...
+	ctx.fillStyle = '#006600';
+	ctx.fillRect(300, 300, 400, 400);
 	
 	// Capture preview after all drawing is done
 	fxpreview();
 }
 
 // Adds 0.1s delay during resize to reduce browser load
-window.onload = draw; var d; window.onresize = function() { clearTimeout(d); d = setTimeout(draw, 100) };
+onload = draw; var d; onresize = function() { clearTimeout(d); d = setTimeout(draw, 100) };
 
 // S - Save PNG
 document.addEventListener('keydown', function(e) {
@@ -41,6 +33,7 @@ document.addEventListener('keydown', function(e) {
 	}
 });
 
+// Features - Use of features is optional
 window.$fxhashFeatures = {
 	'Number of Points' : nPoints
 }
